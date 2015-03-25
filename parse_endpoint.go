7c8bbf922fa894f591c49fc2faeb74cb9e69b2eb
Matt Bruzek <matthew.bruzek@canonical.com>
2015-03-25 13:57:45 -0500

    Making changed base on code review comments.

--- parse_endpoint.go
+++ parse_endpoint.go
@@ -1,36 +1,43 @@
 package virtualservices
 
-import "fmt"
 import "encoding/json"
+import "fmt"
 import "strings"
 
-// Take a sigle enpoint string and parse out the relation, interface, and JSON data.
+type Endpoint struct {
+	relation string
+	iface    string
+	data     string
+}
+
+// ParseEndpoint takes a sigle endpoint string and parses out the relation, interface, and JSON data.
 // relation:interface=JSON
-func ParseEndpoint(str string) (string, string, string, error) {
+func ParseEndpoint(str string) (Endpoint, error) {
+	var endpoint Endpoint
 	relation_index := strings.Index(str, ":")
 	if relation_index == -1 {
-		return "", "", "", fmt.Errorf("No relation index found in %q", str)
+		return endpoint, fmt.Errorf("no relation index found in %q", str)
 	}
-	relation := strings.TrimSpace(str[:relation_index])
-	if relation == "" {
-		return "", "", "", fmt.Errorf("No relation name found in %q", str)
-	}
-
-	splits := strings.Split(str[relation_index+1:], "=")
-	if len(splits) < 2 {
-		return relation, "", "", fmt.Errorf("No interface name found in %q", str)
-	}
-	iface := strings.TrimSpace(splits[0])
-	if iface == "" {
-		return relation, "", "", fmt.Errorf("No interface name found in %q", str)
+	endpoint.relation = strings.TrimSpace(str[:relation_index])
+	if endpoint.relation == "" {
+		return endpoint, fmt.Errorf("no relation name found in %q", str)
 	}
 
-	json_data := strings.TrimSpace(splits[1])
+	interface_index := strings.Index(str, "=")
+	if interface_index == -1 {
+		return endpoint, fmt.Errorf("no interface name found in %q", str)
+	}
+	endpoint.iface = strings.TrimSpace(str[relation_index+1:interface_index])
+	if endpoint.iface == "" {
+		return endpoint, fmt.Errorf("no interface name found in %q", str)
+	}
+	// The JSON data to return is the string, not the map.
+	endpoint.data = strings.TrimSpace(str[interface_index+1:])
 
 	var data map[string]interface{}
-	if err := json.Unmarshal([]byte(json_data), &data); err != nil {
-		return relation, iface, "", fmt.Errorf("Invalid JSON: %q", json_data)
+	if err := json.Unmarshal([]byte(endpoint.data), &data); err != nil {
+		return endpoint, fmt.Errorf("invalid JSON: %q", endpoint.data)
 	}
 
-	return relation, iface, json_data, nil
+	return endpoint, nil
 }
